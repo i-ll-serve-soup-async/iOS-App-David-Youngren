bacc9b725c4d535f27ca0214650e4629995e0901
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-11 12:07:42 -0800

    Make user codable and add register user url post.

--- I'll Serve Soup/I'll Serve Soup/Model Controllers/UserController.swift
+++ I'll Serve Soup/I'll Serve Soup/Model Controllers/UserController.swift
@@ -10,4 +10,43 @@ import Foundation
 
 class UserController {
     
+    let baseURL = URL(string: "https://soup-kitchen-backend.herokuapp.com/api")!
+    
+    func createUser(firstName: String, lastName: String, role: String, email: String, password: String, completion: @escaping (Error?) -> Void) {
+        let newUser = User(firstName: firstName, lastName: lastName, email: email, role: role, password: password)
+        
+        let registerURL = baseURL.appendingPathComponent("staff/register")
+        
+        var urlRequest = URLRequest(url: registerURL)
+        urlRequest.httpMethod = "POST"
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            urlRequest.httpBody = try encoder.encode(newUser)
+        } catch {
+            print("There was an error while posting to the server: \(NSError())")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print(error)
+                completion(error)
+                return
+            }
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+//    func fetchUser(completion: @escaping (Error?) -> Void) {
+//
+//        let staffURL = baseURL.appendingPathComponent("staff")
+//        let idURL = staffURL.appendingPathComponent(<#T##pathComponent: String##String#>)
+//
+//    }
+    
 }
--- I'll Serve Soup/I'll Serve Soup/Models/User.swift
+++ I'll Serve Soup/I'll Serve Soup/Models/User.swift
@@ -8,11 +8,30 @@
 
 import Foundation
 
-struct User {
+struct User: Codable {
     var firstName: String
     var lastName: String
     var email: String
     var role: String
     var password: String
-    var token: String
+    var token: String?
+    var id: Int?
+    
+    init(firstName: String, lastName: String, email: String, role: String, password: String, token: String? = nil, id: Int? = nil) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.role = role
+        self.password = password
+        self.token = token
+        self.id = id
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case firstName = "firstname"
+        case lastName = "lastname"
+        case email = "email"
+        case role = "role"
+        case password = "password"
+    }
 }
