c50ff43d7fc18022aeda66190aeb97d7ad39d17c
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-11 11:00:10 -0800

    Create models.

---
+++ I'll Serve Soup/I'll Serve Soup/Models/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  I'll Serve Soup
+//
+//  Created by Nathanael Youngren on 2/11/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    var name: String
+    var amount: Float
+    var unit: String
+    var imageURL: String
+    var categoryID: Int
+}
---
+++ I'll Serve Soup/I'll Serve Soup/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  I'll Serve Soup
+//
+//  Created by Nathanael Youngren on 2/11/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var firstName: String
+    var lastName: String
+    var email: String
+    var role: String
+    var password: String
+    var token: String
+}
