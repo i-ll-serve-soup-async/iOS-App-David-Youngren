d182a0cf0e319aa95831b155a2c797f3541d1c4e
nateyoungren <nathanaelyoungren@gmail.com>
2019-02-11 11:01:37 -0800

    Initialize model controllers.

---
+++ I'll Serve Soup/I'll Serve Soup/Model Controllers/ItemController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemController.swift
+//  I'll Serve Soup
+//
+//  Created by Nathanael Youngren on 2/11/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import Foundation
+
+class ItemController {
+    
+}
